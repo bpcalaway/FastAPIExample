d6a2006ea16c345ecdf4977a5a0c5b1791e09025
bpcalaway@gmail.com <bpcalaway@gmail.com>
2026-03-05 19:41:41 -0600

    oop

--- src/user.py
+++ src/user.py
@@ -12,9 +12,8 @@ async def get_users(id=None, name=None, is_active=True):
     Returns 1 or many users depending on search criteria, default to only active users
     """
     #session = Session(sql_engine)
-    stmt = select(User)
     with sql_engine.connect() as conn:
-        users = conn.execute(stmt)
+        users = conn.execute(select(User))
 
     return [dict(r._mapping) for r in users]
 
